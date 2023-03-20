a9be1eb431d78f808aa1aa0cf71410f7547bcec8
kwonjonny <thistrik@naver.com>
2023-03-20 18:28:06 +0900

    Project생성
    Java파일 생성

---
+++ javaProject/test/src/test/FIrstJava.java
@@ -0,0 +1,13 @@
+package test;
+
+public class FIrstJava {
+public static void main(String []args) {
+	// TODO Auto-generated method stub
+	
+	
+	
+	System.out.println("First_Java"); 
+	System.out.println("Hello_World");
+	
+}
+}
