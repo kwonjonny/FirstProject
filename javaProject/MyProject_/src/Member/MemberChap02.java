5127c98bff8ca2200d29ab4398ae9a350f615f3f
kwonjonny <thistrik@naver.com>
2023-03-22 13:00:41 +0900

    자바 문제
    자바 문제

--- javaProject/MyProject_/src/Member/MemberChap02.java
+++ javaProject/MyProject_/src/Member/MemberChap02.java
@@ -1,7 +1,18 @@
 package Member;
 
-public class MemberChap02 {
+// 앞서 생성 했던 Member 클래스에 main() 메소드 안에 아래 조건의 변수를 정의해봅시다.
+// String 타입의 이름을 저장할 수 있는 변수 name을 정의해봅시다.
+// int 타입의 나이를 저장할 수 있는 변수 age를 정의해봅시다.
+// double 타입의 키를 저장할 수 있는 변수 height를 정의해봅시다.
+// boolean 타입의 JAVA책의 보유 여부를 저장할 수 있는 변수 hasBook를 정의해봅시다.
+// 이름과 나이, 키, 책의 보유 여부를 출력해봅시다.
+
+public class MemberChap02 { // MemberChap02 에서는 직접 실행하지 않으므로 main메소드를 쓰지 않음
+
     // 변수 정의
+	// 이 변수의 정의에 private 접근 제어자를 넣지 않는 이유는 
+	// 생성자 정의 후 getter에서 인스턴스변수 접근과
+	// setter에서 인스턴스변수의 값을 설정하지 않기 때문
     String name;
     int age;
     double height;
@@ -13,8 +24,9 @@ public class MemberChap02 {
         this.age = age;
         this.height = height;
         this.hasBook = hasBook;
-        // 매개변수 public Member(String name, int age, double height, boolean hasBook) -> name, age, height, hasBook
-        //
+        // 매개변수 public Member(String name, int age, double height, boolean hasBook)
+        // this. -> 이용해 인스턴수 변수 -> name, age, height, hasBook -> this.name의 인스턴수 변수로 변환 
+       
     }
     
     // 출력 메소드 정의
--- javaProject/MyProject_/src/Member/Person.java
+++ javaProject/MyProject_/src/Member/Person.java
@@ -1,5 +1,9 @@
 package Member;
-
+// Person 클래스를 만들어 보고, 아래의 회원 정보를 저장하는 변수들을 선언해봅시다.
+// 변수이름을 작성하는 규칙에 맞게 직접 변수 이름을 정의해 보세요.
+// 회원이름을 저장하는 변수
+// 회원 전화번호 ( 000-0000-0000 )를 저장하는 변수
+// 회원 주민등록번호 ( 000000-0000000 또는 0000000000000 )를 저장하는 변수
 public class Person {
 	
 		// 변수 정의
@@ -64,7 +68,7 @@ public class Person {
 	    }
 }	
 		// print의 메소드안에는 get의 인스턴수 변수를 호출중이고 
-		// 출력시 가독성을 높이기 위해 "Name: " "Phone number: " "Resident number: " 을 사용
+		// 출력시 가독성을 높이기 위해 "Name: " , "Phone number: " , "Resident number: " 을 사용
 
 	class Name { // 클래스의 앞에 public이 안 붙는 이유는 public은 한 파일 내에서만 작성하는것이 정석
 				 // 그럼 Name클래스 안에는 main 메소드를 쓰고 
