5380ec3669149542d1573a148aec37e9059d2ebe
kwonjonny <thistrik@naver.com>
2023-03-22 10:45:28 +0900

    ㅇ
    ㅇ

---
+++ javaProject/FirstJava/src/Chapter02/VariableRefferenceType010.java
@@ -0,0 +1,12 @@
+package Chapter02;
+
+public class VariableRefferenceType010 {
+
+	public static void main(String[] args) {
+		
+		String str = null; // 참조변수 : 객체의 주소값, null -> 아직 주소값을 저장하고있음
+		
+		str = "JAVA";
+	}
+
+}
