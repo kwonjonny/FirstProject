890aa1d64a902928d171ce4eeb4f8702f113a4f7
kwonjonny <thistrik@naver.com>
2023-03-30 13:21:47 +0900

    01 자바 문제 풀이 메인메소드 출력
    01 자바 문제 풀이 메인메소드 출력

--- javaProject/MyProject_/src/Member/MemberChap01.java
+++ javaProject/MyProject_/src/Member/MemberChap01.java
@@ -1,6 +1,7 @@
 package Member;
 
-//Member 클래스를 생성하고, main()메소드를 만들고 자기 자신의 이름을 출력하는 메소드를 만들어 출력해 봅시다.
+//Member 클래스를 생성하고, main()메소드를 만들고 
+//자기 자신의 이름을 출력하는 메소드를 만들어 출력해 봅시다.
 
 public class MemberChap01 {
 
