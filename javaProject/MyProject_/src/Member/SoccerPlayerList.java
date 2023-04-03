9dad84d2932b32b4b3e42789b9341e37cc4063ce
kwonjonny <thistrik@naver.com>
2023-04-03 09:45:33 +0900

    자바 문제 풀이 11 컬렉션 프레임워크 ArrayList,Hashset,TreeSet
    자바 문제 풀이 11 컬렉션 프레임워크 ArrayList,Hashset,TreeSet

--- javaProject/MyProject_/src/Member/SoccerPlayerList.java
+++ javaProject/MyProject_/src/Member/SoccerPlayerList.java
@@ -1,6 +1,9 @@
 package Member;
 
 import java.util.ArrayList;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.HashSet;
 
 // 축구선수 클래스를 만들어 봅시다.
 //	 class FootballPlayer {
@@ -19,7 +22,7 @@ import java.util.ArrayList;
 //그램을 만들어 봅시다. 
 // 
 //4. 축구선수의 번호를 key로 하고 축구선수 인스턴스를 저장하는 Map<K,V> 인스턴스를 이용해서 프로그램을 만들어봅시다
-public class SoccerPlayerList {
+public class SoccerPlayerList implements Comparable<SoccerPlayerList> {
 
 	// 인스턴스 변수 정의
 	private String name;
@@ -39,28 +42,28 @@ public class SoccerPlayerList {
 	public String getName() {
 		return name;
 	}
-	public void setName() {
+	public void setName(String name) {
 		this.name = name;
 	}
 	
 	public int getNumber() {
 		return number;
 	}
-	public void setNumber() {
+	public void setNumber(int number) {
 		this.number = number;
 	}
 	
 	public String getTeam() {
 		return team;
 	}
-	public void setTeam() {
+	public void setTeam(String team) {
 		this.team = team;
 	}
 	
 	public int getAge() {
 		return age;
 	}
-	public void setAge() {
+	public void setAge(int age) {
 		this.age = age;
 	}
 	
@@ -68,6 +71,14 @@ public class SoccerPlayerList {
 	public String toString() {
 		return "선수이름"+name + "선수번호"+number + "소속팀"+team + "선수나이"+age;
 	}
+	
+	 // 3번문제 메소드 
+	 // SoccerPlayerList 클래스를 등번호로 정렬
+    @Override
+    public int compareTo(SoccerPlayerList o) {
+        return this.number - o.getNumber();
+    }
+    
 }	
 
 
@@ -76,14 +87,46 @@ public class SoccerPlayerList {
 class main8 {
 	public static void main(String [] args) {
 	
+	// ArrayList로 배열 출력
 	ArrayList<SoccerPlayerList> soccerPlayer = new ArrayList<>();
-	soccerPlayer.add(new SoccerPlayerList("메시",10,"바르셀로나",36));
+	
+	soccerPlayer.add(new SoccerPlayerList("메시",10,"바르셀로나",35));
 	soccerPlayer.add(new SoccerPlayerList("호날두",07,"알나사르",38));
-	soccerPlayer.add(new SoccerPlayerList("음바페",07,"파리생제르망",23));
+	soccerPlayer.add(new SoccerPlayerList("음바페",07,"파리생제르망",24));
 	soccerPlayer.add(new SoccerPlayerList("손흥민",07,"레알마드리드",30));
 	
 	for(SoccerPlayerList player : soccerPlayer) {
 		System.out.println(player);
 	}
+	
+	 // HashSet으로 중복 체크 후 출력 
+	 Set<SoccerPlayerList> playerSet = new HashSet<>();
+
+     playerSet.add(new SoccerPlayerList("메시", 10, "바르셀로나", 35));
+     playerSet.add(new SoccerPlayerList("호날두",07,"알나사르",38));
+     playerSet.add(new SoccerPlayerList("음바페",07,"파리생제르망",24));
+     playerSet.add(new SoccerPlayerList("손흥민",07,"레알마드리드",30));
+     playerSet.add(new SoccerPlayerList("메시", 10, "바르셀로나", 35)); // 중복이므로 추가되지않음
+     
+     // Set에 저장된 축구선수 인스턴스 출력
+     for (SoccerPlayerList player : playerSet) {
+         System.out.println(player);
+     }
+     
+     // TreeSet으로 중복 체크 후 출력
+     TreeSet<SoccerPlayerList> playerTreeSet = new TreeSet<>();
+
+     playerTreeSet.add(new SoccerPlayerList("메시", 10, "바르셀로나", 35));
+     playerTreeSet.add(new SoccerPlayerList("호날두", 07, "알나사르", 38));
+     playerTreeSet.add(new SoccerPlayerList("음바페", 07, "파리생제르망", 24));
+     playerTreeSet.add(new SoccerPlayerList("손흥민", 07, "레알마드리드", 30));
+     playerTreeSet.add(new SoccerPlayerList("메시", 10, "바르셀로나", 35)); // 중복이므로 추가되지 않음
+
+     // TreeSet에 저장된 축구선수 인스턴스 출력
+     for (SoccerPlayerList player : playerTreeSet) {
+         System.out.println(player);
+     }
+     
 	}
 }
+
