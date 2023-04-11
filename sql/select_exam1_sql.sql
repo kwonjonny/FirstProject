008a61cb88c08c8abfb93846f92cc034b4ebb79b
kwonjonny <thistrik@naver.com>
2023-04-11 17:03:30 +0900

    sql exam
    sql exam

--- sql/select_exam1_sql.sql
+++ sql/select_exam1_sql.sql
@@ -76,9 +76,12 @@ SELECT name, address FROM Customer;
 
 --(4) 2014년7월4일~7월7일사이에주문받은도서의주문번호
 SELECT orderid FROM Orders WHERE orderdate BETWEEN TO_DATE('2014-07-04', 'YYYY-MM-DD') AND TO_DATE('2014-07-07', 'YYYY-MM-DD');
+
 --(5) 2014년7월4일~7월7일사이에주문받은도서를제외한도서의주문번호
  SELECT orderid FROM Orders WHERE orderdate NOT BETWEEN TO_DATE('2014-07-04', 'YYYY-MM-DD') AND TO_DATE('2014-07-07', 'YYYY-MM-DD');
+ 
 --(6) 성이‘김’씨인고객의이름과주소
 SELECT name, address FROM Customer WHERE name LIKE '김%';
+
 --(7) 성이‘김’씨이고이름이‘아’로끝나는고객의이름과주소
 SELECT name, address FROM Customer WHERE name LIKE '김%아';
