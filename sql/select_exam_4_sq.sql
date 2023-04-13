7ebc7601074f8624e907185245908d0e2d3a134e
kwonjonny <thistrik@naver.com>
2023-04-13 17:12:06 +0900

    sql exam
    sql exam

--- sql/select_exam_4_sq.sql
+++ sql/select_exam_4_sq.sql
@@ -1,17 +1,33 @@
 
 --1 마당서점의고객이요구하는다음질문에대해SQL 문을작성하시오.
-
 --(3) 박지성의총구매액(박지성의고객번호는1번으로놓고작성)
-select custid,
-        decode(custid,
-        1, '박지성'
-        ) as name, sum(saleprice) as sum,
-group by custid
-from orders;
+select sum(saleprice)
+from orders
+where custid = 1
+;
+
+select * from customer;
+
+
 --(4) 박지성이구매한도서의수(박지성의고객번호는1번으로놓고작성)
+select count(*) as"박지성씨 구매 도서 수" 
+from orders 
+where custid = 1
+;
+
 
 -- 2 마당서점의운영자와경영자가요구하는다음질문에대해SQL 문을작성하시오.
-
+select * 
+from book;
 --(1) 마당서점도서의총개수
+select count(*)
+from book;
 
 --(2) 마당서점에도서를출고하는출판사의총개수
+-- 출판사 나열
+select count(distinct publisher)
+from book
+order by publisher;
+
+
+
