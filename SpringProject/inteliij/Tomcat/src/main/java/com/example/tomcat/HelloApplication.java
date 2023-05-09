4ef407b11059efff22edeecd889cf27db2fadac3
kwonjonny <thistrik@naver.com>
2023-05-09 11:49:15 +0900

    delete
    delete

--- SpringProject/inteliij/Tomcat/src/main/java/com/example/tomcat/HelloApplication.java
+++
@@ -1,9 +0,0 @@
-package com.example.tomcat;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class HelloApplication extends Application {
-
-}
